34f034f47e80f571f675f72bbe3d3f98b7fadabe
dicoxx <55576920+dicoxx@users.noreply.github.com>
2023-02-28 18:46:27 +0800

    Add files via upload

--- app.R
+++ app.R
@@ -14,16 +14,20 @@ sidebar <- dashboardSidebar(
     id = "sideBar",
     menuItem("Home", tabName = "home", icon = icon("home")),
     menuItem(
-      "Genomic Variation", tabName = "tab2", icon = icon("search"),
+      "Genomic Variation", tabName = "tab2",
       menuItem("Variations by Region", tabName = "tab2_1", icon = icon("calendar-plus")),
       menuItem(
         "Variations in Gene", tabName = "tab2_2", icon = icon("keyboard")
       ),
       menuItem(
-        "Variation by ID:",
+        "Variation by ID",
         tabName = "tab2_3",
         icon = icon("search")
       )
+    ),
+    menuItem(
+      "Transcription factor",
+      tabName = "tab3"
     )
   )
 )
@@ -179,7 +183,62 @@ body <- dashboardBody(
         
       )
       
+    ),
+    #Trans Factor
+    tabItem(
+      tabName = "tab3",
+      fluidPage(
+        titlePanel("Search for Regulation information by TFs:"),
+        br(),
+        fluidRow(
+          column(
+            width = 2,
+            textInput(
+              "tFs",
+              "TFs :",
+              value = "AT1G"
+            )
+          ),
+          column(
+            width = 2,
+            textInput(
+              "tFname",
+              "TFname:",
+              value = "SMB1"
+            )
+          ),
+          column(
+            width = 2,
+            textInput(
+              "annotation",
+              "Annotation:",
+              value = "Promoter"
+            )
+          ),
+          column(
+            width = 2,
+            textInput("targetId", "TargetId:",value = "AT3G26125"),
+          ),
+          column(
+            width = 2,
+            textInput("symbol", "Symbol:",value = "ARF"),
+            
+          ),
+          column(
+            width = 2,
+            div(actionButton(
+              "submit_btn3",
+              "Submit",
+              class = "btn-primary"
+            ),class="my-column")
+          )
+        ),
+        br(),
+        dataTableOutput("results3"),
+        downloadButton("download3", "Download")
+      )
     )
+
    )
   )
     
@@ -196,10 +255,17 @@ ui <- fluidPage(
 )
 
 # Define df outside server function
+#Variation information file
 df <- read_excel("E:\\RWork\\RiceDataBase\\var_example.xlsx")
-#df_Inquire <- read_excel("/srv/shiny-server/snpDB/genes_pos.xlsx")
+#df2_2 <- read_excel("/srv/shiny-server/snpDB/genes_pos.xlsx")
 #df <- read_excel("/srv/shiny-server/snpDB/var_example.xlsx")
-df_Inquire<-read_excel("E:\\RWork\\RiceDataBase\\genes_pos.xlsx")
+#Strand file
+df2_2<-read_excel("E:\\RWork\\RiceDataBase\\genes_pos.xlsx")
+#Transcription and regulation file
+#df3<-read_excel("E:\\RWork\\RiceDataBase\\data\\sub_smb_anno.xlsx")
+df3<-read_excel("E:/RWork/RiceDataBase/data/sub_smb_anno.xlsx")
+
+
 # 定义 server
 server <- function(input, output) {
 
@@ -247,12 +313,12 @@ server <- function(input, output) {
   #tab2_2
   {
     
-    #print(colnames(df_Inquire))
+    #print(colnames(df2_2))
     observe({
       updateSelectInput(
         inputId = "GSN",
-        choices = unique(df_Inquire$GeneID),
-        selected = df_Inquire[2][1]
+        choices = unique(df2_2$GeneID),
+        selected = df2_2[2][1]
       )
     }) 
     
@@ -274,12 +340,12 @@ server <- function(input, output) {
     filtered_data2_2 <- reactiveVal()
     
     observeEvent(eventExpr = input$submit_btn2_2, {
-      row_num <- which(df_Inquire$GeneID == input$GSN)
+      row_num <- which(df2_2$GeneID == input$GSN)
       
-      if(df_Inquire[row_num,"Strand"]==1)
+      if(df2_2[row_num,"Strand"]==1)
       {
-        left<-df_Inquire[row_num,"Start"] - as.numeric(input$Upstream)*1000
-        right<-df_Inquire[row_num,"End"] + as.numeric(input$Downstream)*1000
+        left<-df2_2[row_num,"Start"] - as.numeric(input$Upstream)*1000
+        right<-df2_2[row_num,"End"] + as.numeric(input$Downstream)*1000
         left<-unlist(left)
         left<-as.numeric(left)
         right<-unlist(right)
@@ -317,9 +383,9 @@ server <- function(input, output) {
         }
         
         
-      } else if(df_Inquire[row_num,"Strand"]==0) {
-        left<-df_Inquire[row_num,"Start"] - as.numeric(input$Downstream)*1000
-        right<-df_Inquire[row_num,"End"] + as.numeric(input$Upstream)*1000
+      } else if(df2_2[row_num,"Strand"]==0) {
+        left<-df2_2[row_num,"Start"] - as.numeric(input$Downstream)*1000
+        right<-df2_2[row_num,"End"] + as.numeric(input$Upstream)*1000
         
         left<-unlist(left)
         left<-as.numeric(left)
@@ -329,13 +395,13 @@ server <- function(input, output) {
             &&(is.null(input$EffectRAP) || input$EffectRAP[1] == "")
         )
         {
-          print("douweikong")
+
           filtered_data2_2(df %>%
                              filter(.data[['POS']] >= left) %>%
                              filter(.data[['POS']] <= right))
         }
         else if (is.null(input$EffectMSU) || input$EffectMSU[1] == "")
-        {print("1weikong")
+        {
           filtered_data2_2(df %>%
                              filter(.data[['EffectRAP']]==input$EffectRAP) %>%
                              filter(.data[['POS']] >= left) %>%
@@ -343,7 +409,6 @@ server <- function(input, output) {
         }
         else if (is.null(input$EffectRAP) || input$EffectRAP[1] == "")
         {
-          print("2weikong")
           filtered_data2_2(df %>%
                              filter(.data[['EffectMSU']]==input$EffectMSU) %>%
                              filter(.data[['POS']] >= left) %>%
@@ -420,6 +485,37 @@ server <- function(input, output) {
       }
     )
     
+  }
+  #tab3
+  {
+    # 存储筛选结果
+    filtered_data3 <- reactiveVal()
+    # 当按下“提交”按钮时，筛选表格并更新结果
+    observeEvent(eventExpr = input$submit_btn3, {
+      TFs_data<-df3[grep(input$tFs, df3[['TFs']], ignore.case = TRUE),]
+      TFname_data<-TFs_data[grep(input$tFname, TFs_data[['TFname']], ignore.case = TRUE),]
+      Annotation_data<-TFname_data[grep(input$annotation, TFname_data[['Annotation']], ignore.case = TRUE),]
+      TargetId_data<-Annotation_data[grep(input$targetId, Annotation_data[['TargetId']], ignore.case = TRUE),]
+      Symbol_data<-TargetId_data[grep(input$symbol, TargetId_data[['Symbol']], ignore.case = TRUE),]
+      
+      filtered_data3(Symbol_data)
+    })
+    
+    
+    # 显示结果
+    output$results3 <- renderDataTable({
+      filtered_data3()
+      
+    },options = list(scrollX = TRUE, scrollY = "500px"))
+    
+    output$download3 <- downloadHandler(
+      filename = function() {
+        paste("data-", Sys.Date(), ".csv", sep = "")
+      },
+      content = function(file) {
+        write.csv(filtered_data2(), file)
+      }
+    )
   }
 }
 # 运行应用
